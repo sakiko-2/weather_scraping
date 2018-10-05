196a2c9fcb9671141f1e71c613b93b07e30fbbd6
sakiko <sakiko.ue@gmail.com>
2018-10-05 15:10:29 +1300

    Use BeautifulSoup, get Auckland's weather from nzherald and output to console

--- weather.py
+++ weather.py
@@ -1,5 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
 
-page = requests.get('https://www.nzherald.co.nz/weather-home/')
+URL = 'https://www.nzherald.co.nz/weather-home/'
+page = requests.get(URL)
+soup = BeautifulSoup(page.text, 'html.parser')
 
-print(page)
+auckland = soup.find('h3', text='Auckland').find_parent().find_previous_sibling()
+high = auckland.find(class_='high').get_text()
+low = auckland.find(class_='low').get_text()
+weather = ''.join(auckland.find('img')['class']).capitalize()
+
+print('Auckland')
+print(weather)
+print(high)
+print(low)
