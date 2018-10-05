f577b42083934157d603628ce41280f10c3c99bd
sakiko <sakiko.ue@gmail.com>
2018-10-05 15:45:33 +1300

    Refactor

--- weather.py
+++ weather.py
@@ -5,12 +5,13 @@ URL = 'https://www.nzherald.co.nz/weather-home/'
 page = requests.get(URL)
 soup = BeautifulSoup(page.text, 'html.parser')
 
-auckland = soup.find('h3', text='Auckland').find_parent().find_previous_sibling()
-high = auckland.find(class_='high').get_text()
-low = auckland.find(class_='low').get_text()
-weather = ''.join(auckland.find('img')['class']).capitalize()
+city = 'Auckland'
+regional_data = soup.find('h3', text=city).find_parent().find_previous_sibling()
+high = regional_data.find(class_='high').get_text()
+low = regional_data.find(class_='low').get_text()
+weather = ''.join(regional_data.find('img')['class']).capitalize()
 
-print('Auckland')
+print(city)
 print(weather)
 print(high)
 print(low)
